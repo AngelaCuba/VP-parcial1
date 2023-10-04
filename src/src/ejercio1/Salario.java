3edab044aa15bfd51b66da1305f46162113c36b5
JHOAN <cpf.cuba13394732@gmail.com>
2023-10-04 19:01:49 -0400

    Parcial1 - Commit2

---
+++ src/src/ejercio1/Salario.java
@@ -0,0 +1,18 @@
+package ejercio1;
+
+public class Salario {
+
+    double salarioMinimo=2000;
+    public double descuento(double sueldo) throws Exception{
+        if(sueldo<0){
+            throw Exception(" Esta accion no puede ser realizada");
+        }
+        double descuento=0;
+        if(sueldo>(salarioMinimo*2)){
+            descuento=sueldo*15/100;
+        } else if(sueldo > salarioMinimo){
+            descuento=sueldo*5/100;
+        }
+        return sueldo-descuento;
+    }
+}
