70d439ca92591d6483709284ef0a850310e08ab7
JHOAN <cpf.cuba13394732@gmail.com>
2023-10-04 19:32:59 -0400

    Parcial1 - Commit final

--- src/src/ejerciciomock/Reserva.java
+++ src/src/ejerciciomock/Reserva.java
@@ -1,5 +1,18 @@
 package ejerciciomock;
 
 public class Reserva {
+    Info info= new Info();
 
+    public void setInfo(Info info) {
+        this.info = info;
+    }
+    public String reservar( String destino, int cantidad, int dia, int mes, int gestion){
+        if(dia<1||mes<1||gestion<1||dia>31||mes>12||dia>28 ){
+            return "Esta fecha no es valida";
+        }
+        if (cantidad<1){
+            return "la cantidad no es valida"
+        }
+
+    }
 }
